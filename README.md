6e84766d78f5110a9a6e04108d4894b31468fc03
gimminseog <gm27376@gmail.com>
2020-12-12 22:03:50 +0900

    DOM

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## basics
 
-- [for of, forEach]()
+- [for of, forEach](https://github.com/since-1994/Learn-jacascript/commit/490acb1408edca0134b7db8c1dc27eeb841559f2)
 - [for in]()
 - [scope]()
 
@@ -14,3 +14,8 @@
 
 - [getElementsBy & getElementBy]()
 - [querySelector & querySelectorAll](https://github.com/since-1994/Learn-jacascript/commit/9c1cc73824a471931dbc59fdd44618ce4b58a827)
+- [querySelectorAll 과 getElementsBy의 차이점]()
+  - querySelectorAll을 하면 NodeList를 얻고 getElementsBy를 하면 HTMLCollection을 얻는다.
+  - NodeList는 중간에 item이 추가 되더라도 자동 업데이트를 하지 않는다.
+  - HTMLCollection은 중간에 item이 추가되면 자동으로 업데이트를 한다.
+    **_그렇다면 querySelector를 지양해야 하는가? 그렇지 않다. item들을 포함하는 부모를 찾고 부모의 children을 부르면 알아서 HTMLCollection을 불러주기 때문이다._**
--- dom.js
+++ dom.js
@@ -1,13 +1,9 @@
-const headers = document.getElementsByTagName("h2");
-const list = document.getElementsByClassName("list");
-const button = document.getElementById("button");
-// 이거만 Elements 가 아니라 Element인 것을 볼 수 있다. 제일 처음 찾은 것만 return 한다.
+const collectionItems = document.getElementsByClassName("item");
+const list = document.querySelector(".list");
 
-const item1 = document.querySelector(".item");
-// 이거는 getElement와 동일하게 제일 처음 elemnt만 return 한다.
-const items = document.querySelectorAll(".item");
-
-const h1 = document.querySelector("h2.item");
-
-
-console.log(headers);
+console.log(list.childNodes);
+// 이거는 list의 자식 중에서 element만 반환하는게 아니라 주석 , text 모두 반환한다.
+// 만약 element만 반환하게 하고 싶다면 children 사용
+// HTMLcollction은 forEach method 사용할 수 없음.
+// NodeList는 사용 가능
+console.log(list.children);
