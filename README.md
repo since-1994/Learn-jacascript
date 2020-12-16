3c981a2186b89818b467145215eb89fea3979fb4
gimminseog <gm27376@gmail.com>
2020-12-17 00:59:45 +0900

    todo-app

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## basics
 
 - [for of, forEach](https://github.com/since-1994/Learn-jacascript/commit/490acb1408edca0134b7db8c1dc27eeb841559f2)
-- [for in]()
+- [for in](https://github.com/since-1994/Learn-jacascript/commit/9c7d7d4eabeaf9422546ab888ff419d89d505375)
 - [scope]()
 
 ## DOM
@@ -21,6 +21,41 @@
   - HTMLCollection은 중간에 item이 추가되면 자동으로 업데이트를 한다.
 
     **그렇다면 querySelector를 지양해야 하는가? 그렇지 않다. item들을 포함하는 부모를 찾고 부모의 children을 부르면 알아서 HTMLCollection을 불러주기 때문이다.**
+
+- [NodeList]()
+  어떤 element의 element.childNodes를 하면 childNodes는 text까지 불러오기 때문에 조심해야 한다. 아래 예시를 보자
+
+```html
+<div class="parent"></div>
+```
+
+```javascript
+const parent = document.querySelector(".parent");
+const children = parent.childNodes; //NodeList를 반환
+```
+
+위와 같았다면 childNodes는 공백도 가져오기 때문에 주의를 해야 한다.
+
+- [Event 종류]()
+
+  - transitionend
+    언제 쓰면 좋냐면 특정 에니매이션이 실행된 뒤에 실제 코드가 돌아가길 원할때 유용하게 사용할 수 있다.예시는 아래와 같다.
+
+  ```javascript
+  target.classList.add("fall");
+  target.addEventListener("transitionend", function () {
+    target.remove();
+  });
+  ```
+
+  위의 코드는 떨어지는 애니메이션 뒤에 실제 삭제가 되도록 하는 코드이다.
+
+  - DOMContentLoaded
+    이 이벤트는 HTML 문서를 완전히 불러오고 분석했을 떄 실행됩니다. localStorage에서 뭐 불러오는 트리거로 사용하기 좋습니다. 아래와 같이 사용할 수 있습니다.
+
+  ```javascript
+  document.addEventListener("DOMContentLoaded", load);
+  ```
 
 - [Events](./events.js)
 
@@ -78,3 +113,86 @@ div.addEventListener("click", function (event) {
 
   - 두 storage의 차이점
     localStorage는 브라우저를 재시동해도 날아가지 않는다. sessionStorage는 날아간다.
+
+## ARRAY
+
+- HIGHER FUNCTION
+  Higher function이란 다른 함수(메서드)를 parameter로 받는 것을 의미한다.
+
+```javascript
+function repeater(fn) {
+  fn();
+  fn();
+  fn();
+}
+```
+
+- CALLBACK METHOD
+  Higher function에 인자로 들어가는 함수를 callback 함수라고 한다. 그니까 바로 실행되는게 아니고 특정한 호출에 의해서 실행되는 함수를 callback 함수라고 생각하면 된다.
+
+- MAP
+  MAP은 reference type을 복사하는데 사용된다. 아래 예시를 보자.
+
+```javascript
+const videos = [1, 2, 3, 4];
+cosnt newVideos = videos.map(function(video){
+  return video+1;
+});
+
+console.log(newVideos);//[2, 3, 4, 5]
+```
+
+만약에 return을 해주지 않더라도 map은 항상 return을 해주기 때문에 [undefined, undefined, undefined]이런식으로 된다. 이것은 완전복사라서 원래 배열에 영향을 미치지 않는다.
+
+- FIND
+  find 메서드는 조건에 맞는 첫번째 소자를 return 해줍니다.
+
+```javascript
+const videos = ["hello", "hi", "fancy video"];
+
+const searchResult = videos.find(function (video) {
+  return video.includes("fancy");
+});
+
+console.log(searchResult); //fancy video
+```
+
+- FILTER
+  filter는 find와 비슷한 역할을 하지만 multiple value를 반환해줍니다.
+
+```javascript
+const videos = [1, 2, 3, 4];
+
+const result = videos.filter(function (video) {
+  return video < 3;
+});
+
+console.lod(result); // [1, 2];
+```
+
+- SOME and EVERY
+  먼저 some과 every는 bool 을 반환하는 메서드다. 아래 예시를 보자.
+
+```javascript
+const numbers = [1, 2, 3, 4];
+
+const bigger = numbers.every(function (number) {
+  return number > 2;
+});
+
+console.log(bigger); //false
+```
+
+어떤 조건에 대해 어떤 value만 만족하는지 확인하려면 `some`을 사용하고 모든 value가 만족하는지 확인하려면 `every`를 사용하자.
+
+- ARROW FUNCTION
+
+```javascript
+const games = ["minecraft", "pockemon"];
+
+const arraowGame = games.map((game) => game + "2");
+
+console.log(arrowGame); // ["minecraft2", "pockemon2"]
+```
+
+위에 예시처럼 arrow function에서 만약 return 값만 필요하다면 `return`을 생략하고 적을 수도 있다.
--- app.js
+++ app.js
@@ -2,10 +2,39 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOptions =document.querySelector('.filter-todo');
+const TODOS =  'todos';
 
-
+document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener('click', checkDelete);
+filterOptions.addEventListener('change', filterTodo);
+
+function filterTodo(e){
+    const todos = todoList.children;
+    console.log(todos);
+   todos.forEach(function(todo){
+       switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("todo-item--complete")){
+                    todo.style.display = "flex";
+                }else{
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if(todo.classList.contains("todo-item--complete")){
+                    todo.style.display = "none";
+                }else{
+                    todo.style.display = "flex";
+                }
+                break;
+       }
+   });
+}
 
 function addTodo(event){
     //Preven form from submitting
@@ -16,6 +45,7 @@ function addTodo(event){
 
     const newTodo = document.createElement('li');
     newTodo.innerText = todoInput.value;
+    saveTodos(todoInput.value);
     todoInput.value = "";
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
@@ -38,9 +68,66 @@ function checkDelete(event){
     const target = item.parentElement;
     
     if(item.classList[0] === 'trash-btn'){
-        target.remove();
+        target.classList.add('fall');
+        removeTodos(target.children[0].innerText);
+        target.addEventListener('transitionend', function(){
+            target.remove();
+        })
     }else if(item.classList[0] === 'complete-btn'){
        target.classList.toggle('todo-item--complete');
        console.log(target);    
     }
+}
+
+function saveTodos(todo){
+    let todos = JSON.parse(localStorage.getItem(TODOS));
+
+    if(todos === null){
+        todos = [];
+        todos.push(todo);
+    }else{
+        todos.push(todo);
+    }
+
+    localStorage.setItem(TODOS, JSON.stringify(todos));
+}
+
+function getTodos(){
+    let todos = JSON.parse(localStorage.getItem(TODOS));
+
+    if(todos != null){
+        todos.forEach(function(todo){
+            const todoDiv = document.createElement('div');
+            todoDiv.classList.add('todo')
+        
+            const newTodo = document.createElement('li');
+            newTodo.innerText = todo;
+            newTodo.classList.add('todo-item');
+            todoDiv.appendChild(newTodo);
+        
+            const completedButton = document.createElement('button');
+            completedButton.innerHTML = '<i class ="fas fa-check"></i>'
+            completedButton.classList.add('complete-btn')
+            todoDiv.appendChild(completedButton);
+        
+            const trashButton = document.createElement('button');
+            trashButton.innerHTML = '<i class ="fas fa-trash"></i>'
+            trashButton.classList.add('trash-btn')
+            todoDiv.appendChild(trashButton);
+        
+            todoList.appendChild(todoDiv);        
+        })
+    }
+
+}
+
+function removeTodos(todo){
+    let todos = JSON.parse(localStorage.getItem(TODOS));
+
+    if(todos != null){
+        const targetIdx = todos.indexOf(todo);
+        todos.splice(targetIdx, 1);
+    }
+
+    localStorage.setItem(TODOS, JSON.stringify(todos));
 }
